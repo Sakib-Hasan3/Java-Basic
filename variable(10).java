24f7592e8c78d6e0fbfa25bb4e687330956f002b
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2023-10-28 04:44:57 +0600

    Create variable(10).java
    https://youtu.be/8Udh2XRF2gc?si=3L0Ez1xLWfmuOpHJ

---
+++ variable(10).java
@@ -0,0 +1,6 @@
+Variable:  a variable is a reserved memory location to store values. Variables in Java are created at the time of declaration and the size of the variable is determined by the type of the variable.
+
+Here is an example of a variable declaration in Java:
+int numberOfStudents;
+
+In this example, numberOfStudents is a variable of type int.
