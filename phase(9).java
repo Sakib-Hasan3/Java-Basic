23cf5fd15b85d0ed17d072a21ec0f38856b56212
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2023-10-28 04:42:31 +0600

    Create phase(9).java
    https://youtu.be/IKIVEPRW6uM?si=kgFM8KA-shaFjvIe

---
+++ phase(9).java
@@ -0,0 +1,12 @@
+1. Translation
+2. Compilation
+3. Runtime
+4. Interpretation
+5. Garbage Collection
+
+  Example 
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
