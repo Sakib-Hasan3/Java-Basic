548df274096d007e63acb744c9bb3e14e425414c
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2023-10-28 04:39:10 +0600

    Create comment(8).java
    https://youtu.be/jZlMh69-45c?si=6f4fjQHytvKBc0GI

---
+++ comment(8).java
@@ -0,0 +1,15 @@
+
+Single Line Comment ::
+  // This is a single line comment in Java
+  public class Main {
+    public static void main(String[] args) {
+        // This is a single line comment in Java
+        System.out.println("Hello, World!");
+    }
+}
+
+Multiple line comment ::
+  To add multiple lines of comments in Java, you can use the /* ... */ syntax.
+  /* This is a multiple line comment in Java.
+   You can write as many lines as you want.
+*/
