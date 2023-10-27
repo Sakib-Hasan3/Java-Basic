7f014c51934e6ac51a5191a24b1d22c56c0f88ad
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2023-10-28 04:46:30 +0600

    Create datatype(10).java
    https://youtu.be/8Udh2XRF2gc?si=3L0Ez1xLWfmuOpHJ

---
+++ datatype(10).java
@@ -0,0 +1,8 @@
+Data Type: In Java, every variable has a specific type. The type of a variable defines the kind of value that can be stored in a variable. Java is a strongly typed language, which means variables cannot change their type after they have been declared.
+
+int:  32-bit signed integer.
+float:  a single-precision 32-bit IEEE 754 floating point.
+double:  a double-precision 64-bit IEEE 754 floating point.
+char:  a single character.
+boolean:a boolean value, either true or false.
+Note: In Java, the type of the variable must be explicitly declared, oth
