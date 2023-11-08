cb105c02716805e2d5a0b48c6233b243dd9f7363
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2023-11-08 17:58:08 +0600

    Create Assignment(17).java
    https://youtu.be/YDbN8R00vY0?si=I5UfA_vC7Rx8_qEe

---
+++ Assignment(17).java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        System.out.println("Value of num1 before assignment: " + num1);
+        num1 = num2; // assigning the value of num2 to num1
+        System.out.println("Value of num1 after assignment: " + num1);
+    }
+}
+
+output :
+Value of num1 before assignment: 10
+Value of num1 after assignment: 20
+
+  /* In this code, the value of num2 (20) is assigned to num1*/
