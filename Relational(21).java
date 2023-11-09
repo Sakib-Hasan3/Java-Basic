b149af7a89d457a9f3d1f71702c26803b33a19ae
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2023-11-09 18:34:45 +0600

    Create Relational(21).java
    https://youtu.be/eSU34np9DLc?si=jNaDM1q62HR3StI4

---
+++ Relational(21).java
@@ -0,0 +1,41 @@
+/* Equal to (==): This operator is used to compare two operands. If both operands are equal, it returns true; otherwise, it returns false.
+java */
+int a = 10;
+int b = 10;
+System.out.println(a == b); // Output: true
+
+/* Not equal to (!=): This operator is used to compare two operands. If both operands are not equal, it returns true; otherwise, it returns false.
+java */
+
+int a = 10;
+int b = 20;
+System.out.println(a != b); // Output: true
+
+/* Greater than (>): This operator is used to compare two operands. If the first operand is greater than the second operand, it returns true; otherwise, it returns false.
+java */
+
+int a = 20;
+int b = 10;
+System.out.println(a > b); // Output: true
+
+/* Less than (<): This operator is used to compare two operands. If the first operand is less than the second operand, it returns true; otherwise, it returns false.
+java */
+
+int a = 10;
+int b = 20;
+System.out.println(a < b); // Output: true
+
+/* Greater than or equal to (>=): This operator is used to compare two operands. If the first operand is greater than or equal to the second operand, it returns true; otherwise, it returns false.
+java */
+
+int a = 20;
+int b = 10;
+System.out.println(a >= b); // Output: true
+
+/* Less than or equal to (<=): This operator is used to compare two operands. If the first operand is less than or equal to the second operand, it returns true; otherwise, it returns false.
+java */
+
+int a = 10;
+int b = 20;
+
+
