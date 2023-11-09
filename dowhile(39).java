5ca8352ef6b425e2c253499620065c5deb8ae031
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2023-11-09 19:13:16 +0600

    Create dowhile(39).java
    https://youtu.be/sDNrL8xoRx0?feature=shared

---
+++ dowhile(39).java
@@ -0,0 +1,37 @@
+
+
+/*
+do while loop syntax
+----------------
+    initialization;
+   do
+    {
+        statement(s)
+        increment/decrement
+    }while(condition);
+*/
+public class dowhileloop 
+{
+    public static void main(String[] args) 
+    {
+        int i=1;
+        do
+        {
+            System.out.println("Bangladesh");
+            i++;
+        }while(i<=10);
+    }
+}
+/*
+Bangladesh
+Bangladesh
+Bangladesh
+Bangladesh
+Bangladesh
+Bangladesh
+Bangladesh
+Bangladesh
+Bangladesh
+Bangladesh
+Bangladesh
+  */
