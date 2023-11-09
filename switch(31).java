5a0949565cd556e3339f9a94ff1ea3c77e746056
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2023-11-09 18:56:27 +0600

    Create switch(31).java
    https://youtu.be/NDtRRR3WDL0?feature=shared

---
+++ switch(31).java
@@ -0,0 +1,45 @@
+public class Main {
+    public static void main(String[] args) {
+        int day = 5;
+        int month = 10;
+
+        switch (day) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Weekday");
+                break;
+            case 6:
+            case 7:
+                System.out.println("Weekend");
+                break;
+            default:
+                System.out.println("Invalid day");
+        }
+
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                System.out.println("31 days");
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                System.out.println("30 days");
+                break;
+            case 2:
+                System.out.println("28 or 29 days");
+                break;
+            default:
+                System.out.println("Invalid month");
+        }
+    }
+}
