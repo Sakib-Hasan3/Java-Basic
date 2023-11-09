55b8c932ac6d9d7b5d6401a2b064a33bde290bd0
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2023-11-09 19:11:38 +0600

    Create while(38).java
    https://youtu.be/e3GWjcY478Y?feature=shared

---
+++ while(38).java
@@ -0,0 +1,33 @@
+/*
+while loop syntax
+----------------
+    initialization;
+    while(condition)
+    {
+        statement(s)
+        increment/decrement
+    }
+*/
+public class whileloop
+{
+    public static void main(String[] args) {
+        int i=1;
+        while(i<=10)
+        {
+            System.out.println("Bangladesh");
+            i++;
+        }
+    }
+}
+/*
+Bangladesh
+Bangladesh
+Bangladesh
+Bangladesh
+Bangladesh
+Bangladesh
+Bangladesh
+Bangladesh
+Bangladesh
+Bangladesh
+*/
