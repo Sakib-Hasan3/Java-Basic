f29437b6ccc12f2e51395f373bf3e0791e2cec02
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2023-11-15 11:18:27 +0600

    Create foreach(73).java
    https://youtu.be/1LIbHvJX-1w?feature=shared

---
+++ foreach(73).java
@@ -0,0 +1,33 @@
+//video 74
+/*
+
+public class foreach {
+    public static void main(String[] args) {
+        String name[]={"Sakib","Arif","Atik"};
+        for(int i=0;i<5;i++)
+        {
+            System.out.println(name[i]);
+        }
+    }
+}
+*/
+
+public class foreach
+{
+    
+    public static void main(String[]args)
+    {
+        int num[]={10,20,30,40,50};
+        for(int i=0;i<5;i++) //for(int x: num)
+        {
+            System.out.println(num[i]); //System.out.println(x);
+        }
+    }
+}
+
+output 
+  10
+20
+30
+40
+50
